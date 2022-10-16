a77ed20cd8d87041d29680484ee029e4ea114cd9
carolinyAbreu <caroliny.aurem@gmail.com>
2022-10-16 19:05:36 -0300

    update questao 1 - relatorio 8

--- EscolaDB.py
+++ EscolaDB.py
@@ -7,15 +7,15 @@ class Person(object):
         self.db = Graph(uri='bolt://34.201.33.19:7687',
                         user='neo4j', password='bend-humps-force')
 
-    def create(self, teacher):
+    def createTeacher(self, teacher):
         return self.db.execute_query('CREATE (n:Teacher {name:$name, age:$age, area:$area}) return n',
                                      {'name': teacher['name'], 'age': teacher['age'], 'area': teacher['area']})
     
-    def create(self, classes):
+    def createClass(self, classes):
         return self.db.execute_query('CREATE (n:Classes {topic:$topic, time:$time}) RETURN n',
-                                     {'topic': classes['topic'], 'time': teacher['time']})
+                                     {'topic': classes['topic'], 'time': classes['time']})
     
     def create_relation(self, teacher, classes, year):
         return self.db.execute_query('MATCH (n:Teacher {name:$name}), (m:Classes {topic:$topic}) CREATE (n)-[r:TEACH{year: $year}]->(m) RETURN n, r, m',
-                                     {'name': teacher['name'], 'topic': classes['topic'], 'year': year})   
+                                     {'name': teacher['name'], 'topic': classes['topic'], 'year': year['year']})   
 
